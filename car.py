8501d0a197da52809750485ee013ce3a9d7782aa
Erfan.computer <TimorshahTahib@gmail.com>
2022-03-16 13:18:40 +0330

    ...

--- car.py
+++ car.py
@@ -4,11 +4,13 @@ from turtle import color
 class car:
     color="red"
     
-    def getcolor(self):
-     return self.color
+    def getcolor(self,color):
+  
+        self.color=color
+        return   self.color  
      
 
 myob=car()
 
 
-print(myob.getcolor())
+print(myob.getcolor("golsd"))
