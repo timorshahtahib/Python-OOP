4d4b60fcd101fcc8941043d503617e1c04182a47
Erfan.computer <TimorshahTahib@gmail.com>
2022-03-16 16:15:58 +0330

    tosting_methode

--- spicific_function.py
+++ spicific_function.py
@@ -1,6 +1,19 @@
+import re
+
+
 class Point:
     
     def __init__(self,a,b):
         self.a=a
         self.b=b
       
+    def __str__(self):
+        
+        return "Point x={}, Point Y={}".format(self.a,self.b)
+    
+    
+    
+
+
+p=Point(12,13)
+print(p)
